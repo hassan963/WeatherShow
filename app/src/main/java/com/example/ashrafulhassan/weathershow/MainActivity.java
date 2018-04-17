f0f6859eb1873e3d45d2df79b4b17759a98dee7b
hassanmashraful <hassan963@outlook.com>
2018-04-17 13:14:31 +0600

    time done

--- app/src/main/java/com/example/ashrafulhassan/weathershow/MainActivity.java
+++ app/src/main/java/com/example/ashrafulhassan/weathershow/MainActivity.java
@@ -1,37 +1,37 @@
 package com.example.ashrafulhassan.weathershow;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import com.github.clans.fab.FloatingActionButton;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.bumptech.glide.Glide;
 import com.example.ashrafulhassan.weathershow.network.VolleyRequest;
+import com.github.clans.fab.FloatingActionMenu;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 import xyz.matteobattilana.library.WeatherView;
 
@@ -40,9 +40,11 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView dateTextView, locationTextView, weatheverdict, tempTextView, maxTextView,
             minTextView, speedTextView, humidityTextView, degreeTextView, sunriseTextView,
-            sunsetTextView, status;
+            sunsetTextView, status, timeTextView;
 
     private String main, description, temp, pressure, humidity, temp_min, temp_max, speed, deg, sunrise, sunset;
+    private FloatingActionButton menu1, menu2;
+    private FloatingActionMenu floatingActionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
+        floatingActionMenu = findViewById(R.id.menu);
 
         WeatherView mWeatherView = findViewById(R.id.weather);
         mWeatherView.setWeather(WeatherView.weatherStatus.RAIN);
@@ -67,7 +70,11 @@ public class MainActivity extends AppCompatActivity {
         degreeTextView = findViewById(R.id.degreeTextView);
         sunriseTextView = findViewById(R.id.sunriseTextView);
         sunsetTextView = findViewById(R.id.sunsetTextView);
+        timeTextView = findViewById(R.id.timeTextView);
         status = findViewById(R.id.status);
+
+        menu1 = findViewById(R.id.menu_item1);
+        menu2 = findViewById(R.id.menu_item2);
 
 
         String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
@@ -78,13 +85,72 @@ public class MainActivity extends AppCompatActivity {
                 .into(weatherstatus);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.menu_item);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+                *//*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*//*
                 requestVolley("dhaka");
+            }
+        });*/
+
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+                builder.setView(dialogView);
+                final EditText editTestValueMax = dialogView.findViewById(R.id.editTestValueMax);
+                editTestValueMax.setHint("my city name, country code");
+                builder.setTitle("City Name");
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String value = editTestValueMax.getText().toString();
+                        requestVolley(value);
+                        floatingActionMenu.close(true);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        floatingActionMenu.close(true);
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+                builder.setView(dialogView);
+                final EditText editTestValueMax = dialogView.findViewById(R.id.editTestValueMax);
+                editTestValueMax.setHint("my country code");
+                builder.setTitle("Country Code");
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String value = editTestValueMax.getText().toString();
+                        requestVolleyTime(value);
+                        floatingActionMenu.close(true);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        floatingActionMenu.close(true);
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
             }
         });
     }
@@ -122,6 +188,7 @@ public class MainActivity extends AppCompatActivity {
         sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+6"));
         String formattedDate = sdf.format(date);
 
+
         return formattedDate;
 
 
@@ -130,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
     private void requestVolley(final String cityName){
 
 
-        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://api.openweathermap.org/data/2.5/weather?q="+ cityName + ",bd&units=metric&appid=1efc0fc4375a4a874205759effcc7e36",
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=metric&appid=1efc0fc4375a4a874205759effcc7e36",
                 (String) null, new com.android.volley.Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -192,5 +259,39 @@ public class MainActivity extends AppCompatActivity {
 
         VolleyRequest.getInstance().addToRequestQueue(jsonObjectRequest);
 
+    }
+
+    private void requestVolleyTime(final String countryCode){
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "http://api.timezonedb.com/v2/list-time-zone?key=SU17ZY5OYTE7&format=json&country=" + countryCode,
+                (String) null, new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("zones");
+                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+                    String gmtOffset = jsonObject.getString("gmtOffset");
+                    String timestamp = jsonObject.getString("timestamp");
+                    String countryName = jsonObject.getString("countryName");
+
+                    Long gmt = Long.parseLong(gmtOffset); Long time = Long.parseLong(timestamp);
+                    time = time - gmt;
+                    //convertMillis(time);
+                    timeTextView.setText(String.valueOf(convertMillis(time)));
+                    //Toast.makeText(MainActivity.this, ""+convertMillis(time), Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        VolleyRequest.getInstance().addToRequestQueue(jsonObjectRequest);
     }
 }
